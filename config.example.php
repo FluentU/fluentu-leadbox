007cdcf91de06b11ed702ece9c62e0ede2a7b8ca
Elco Brouwer von Gonzenbach <elco.brouwer@gmail.com>
2024-12-19 12:04:12 +0700

    Make insert point match with updated CSS classes

--- config.example.php
+++ config.example.php
@@ -10,4 +10,4 @@ define('PRINTFRIENDLY_CSS_URL', get_stylesheet_directory_uri() . '/css/printfrie
 define('EO_API_URL', '');
 define('EO_API_KEY', '');
 define('EO_LIST_ID', 5);
-define('INSERT_POINTS', ['<h2', '<div class="fluen-after-content"']);
+define('INSERT_POINTS', ['<h2', '<div class="fluen-after-content']);
