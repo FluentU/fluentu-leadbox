624e38a98870d93f13fbd2928bf435fa5b36f4b4
Elco Brouwer von Gonzenbach <elco.brouwer@gmail.com>
2024-12-23 12:29:32 +0700

    Bump version; update config example

--- fluentu-leadbox.php
+++ fluentu-leadbox.php
@@ -9,7 +9,7 @@
  * Plugin Name:       FluentU LeadBox Plugin
  * Plugin URI:        https://github.com/FluentU/fluentu-leadbox
  * Description:       Simple plugin for generating PDFs from posts and emailing download links.
- * Version:           2.7.3
+ * Version:           3.0.0
  * Author:            Elco Brouwer von Gonzenbach
  * Author URI:        https://github.com/elcobvg
  * Text Domain:       fluentu-leadbox
